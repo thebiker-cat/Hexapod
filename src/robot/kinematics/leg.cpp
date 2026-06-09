729083fb76db5ab813c3508a56c36835943006aa
podla <jpodlas@student.tgm.ac.at>
2026-06-10 00:19:01 +0200

    Added comments on most of the new code structure. Changed some pulse-settings in the setAngles-method as well.

--- src/robot/kinematics/leg.cpp
+++ src/robot/kinematics/leg.cpp
@@ -1,17 +1,38 @@
-#include "leg.h"
-#include <Adafruit_PWMServoDriver.h>
+#include "leg.h" // This is the header file, which includes the Leg class
+#include <Adafruit_PWMServoDriver.h> // This is the PCA9685 servo driver with 16 channels
 
+/**
+ * This is the constructor of a leg object, which sets a PCA9685 servo driver and 3 channels for the 3 motors of one leg, which
+ * are on this board as the class attributes.
+ * @param board , this is the PCA9685 servo driver, which has 16 PWM channels on it
+ * @param ch1 , this is the channel for the coxa - servo
+ * @param ch2 , this is the channel for the femur - servo
+ * @param ch3 , this is the channel for the tibia - servo
+ */
 Leg::Leg(Adafruit_PWMServoDriver& board, int ch1, int ch2, int ch3)
-    : _board(board), _ch1(ch1), _ch2(ch2), _ch3(ch3) {
+    : _board(board), _ch1(ch1), _ch2(ch2), _ch3(ch3) { // this is a list for setting all the class attributes immediately
 }
 
+/**
+ * This is a methode to set the different angles of the 3 servo motors on each leg.
+ * The angles are being transformed in pulses (angleToPulse), because the servo works with PWM pulses.
+ * @param coxa , this is the param to control the coxa angle of the servo, which is the angle between the body and the first segment of the leg.
+ * @param femur , this is the param to control the femur angle of the servo, which ist the angle between the first and second segment of the leg.
+ * @param tibia , this is the param to control the tibia angle of the servo, which ist the angle between the second and third segment of the leg.
+ */
 void Leg::setAngles(float coxa, float femur, float tibia) {
+    // _board is the class-attribute for the Servo-Driver
     _board.setPWM(_ch1, 0, angleToPulse(coxa));
     _board.setPWM(_ch2, 0, angleToPulse(femur));
     _board.setPWM(_ch3, 0, angleToPulse(tibia));
 
 }
 
+/**
+ * This is a method to change an angle in degrees into a PWM-pulse format.
+ * @param angle , this is the param, which gives degrees as a float.
+ * @return returns an int value
+ */
 int Leg::angleToPulse(float angle) {
-    return map(angle, 0, 180, 150, 600);
+    return map(angle, 0, 180, 205, 410); // 0° --> 205 ticks, 180° --> 410 ticks
 }
--- src/robot/main_robot.cpp
+++ src/robot/main_robot.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
-#include <Adafruit_PWMServoDriver.h>
-#include "kinematics/leg.h"
+#include <Adafruit_PWMServoDriver.h> // This includes the library for the servo driver
+#include "kinematics/leg.h" // This includes the library for creating a LEG-object
 
-Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40); // first servo driver for the first 3 legs (9 servos)
+Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41); // second servo driver for the last 3 legs (9 servos)
 
+// These are all 6 legs defined with the different channels and boards their servos are on, so they can be adressed
 Leg frontLeft(board1, 0, 1, 2);
 Leg frontRight(board1, 3, 4, 5);
 Leg midLeft(board1, 6, 7, 8);
@@ -13,10 +14,10 @@ Leg backLeft(board2, 3, 4, 5);
 Leg backRight(board2, 6, 7, 8);
 
 void setup() {
-    board1.begin();
-    board1.setPWMFreq(50);
-    board2.begin();
-    board2.setPWMFreq(50);
+    board1.begin(); // starts the first servo-driver
+    board1.setPWMFreq(50); //sets the PWM-frequency of the first servo-driver to 50 Hz
+    board2.begin(); // starts the second servo-driver
+    board2.setPWMFreq(50); //sets the PWM-frequency of the second servo-drover to 50 Hz
 }
 
 void loop() {
