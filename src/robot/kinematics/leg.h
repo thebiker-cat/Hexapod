7148bbb7a5b9e6797e8c0c3c11497a270d968736
jauszerleitner <267324214+jauszerleitner@users.noreply.github.com>
2026-06-10 14:33:26 +0200

    added servo test and changed angleToPulse to public and static
    Signed-off-by: jauszerleitner <267324214+jauszerleitner@users.noreply.github.com>

--- src/robot/kinematics/leg.h
+++ src/robot/kinematics/leg.h
@@ -11,10 +11,9 @@ class Leg {
 public:
     Leg(Adafruit_PWMServoDriver& board, int ch1, int ch2, int ch3); // The constructor delivers the servo-driver and the needed channels on it for the servos
     void setAngles(float coxa, float femur, float tibia); // This method is a setter-method for the angles of all the 3 servos as PWM-pulses
-
+    static int angleToPulse(float angle); // This is a method to create a PWM-pulse out of degrees
 //This is where all the private class-attributes and methods are defined
 private:
     Adafruit_PWMServoDriver& _board; // This is the PCA9685 servo driver, for adressing all the different servos
     int _ch1, _ch2, _ch3; // These are the 3 channels on the servo driver for the 3 different servos of one leg
-    int angleToPulse(float angle); // This is a method to create a PWM-pulse out of degrees
 };
--- src/tests/potentiometer-thebiker_cat.cpp
+++ src/tests/potentiometer-thebiker_cat.cpp
@@ -1,3 +1,21 @@
-//
-// Created by jakob on 10.06.2026.
-//
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include "robot/kinematics/leg.h"
+
+#define SERVO_PIN 2
+
+Servo servo;
+
+
+void setup() {
+    servo.setPeriodHertz(50);
+    servo.attach(SERVO_PIN, 0, 2400);
+    servo.write(90);
+}
+
+void loop() {
+    //Leg::angleToPulse();
+    servo.write(0);
+    delay(1000);
+    servo.write(180);
+}
